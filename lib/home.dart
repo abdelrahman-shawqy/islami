6b86fd72f8dbc97d82d5d498cecce5f580b2c2ec
abdelrahman <aabdelrahmann.sshawqqy.1482000@gmail.com>
2025-12-17 12:54:38 +0200

    home screen added

---
+++ lib/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  static const String routeName ="Home";
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
